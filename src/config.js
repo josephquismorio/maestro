cb43e229a68d7fa56d61efc1fec28f8f30b3500f
jfquismorio <107204228+jfquismorio@users.noreply.github.com>
2023-04-18 21:29:26 -0500

    lol

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "9c387c2ff1644ff09e23b676a5b9bf2e";
-export const redirectUri = "https://maestro-six.vercel.app/callback";
+export const redirectUri = "https://maestro-six.vercel.app";
 export const scopes = [
     "user-library-read",
     "user-top-read",
